145a15c270b3ab61aa08458a209c93cba72c83a3
kumustaka <sbeaury@gmail.com>
2020-02-16 19:09:30 -0500

    add build files

---
+++ build/precache-manifest.9df38f32935080f5718b8285d1dea7e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc5b83b70fef9a59593d",
+    "url": "/static/css/main.665bdb7d.chunk.css"
+  },
+  {
+    "revision": "dc5b83b70fef9a59593d",
+    "url": "/static/js/main.b96c2551.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ef886085179d1250471",
+    "url": "/static/js/2.c805f9ee.chunk.js"
+  },
+  {
+    "revision": "66ab9900b84047efb6be460ca7dc907e",
+    "url": "/index.html"
+  }
+];
